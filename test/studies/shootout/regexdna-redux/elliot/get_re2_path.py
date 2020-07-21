5d0c7021810a14121d08732754e4735240cfd6a0
Elliot Ronaghan <ronawho@gmail.com>
2020-07-21 11:34:24 -0400

    Update get_re2_path.py

--- test/studies/shootout/regexdna-redux/elliot/get_re2_path.py
+++ test/studies/shootout/regexdna-redux/elliot/get_re2_path.py
@@ -3,15 +3,15 @@
 import os
 import sys
 
-util_dir = os.path.join(os.getenv('CHPL_HOME'), 'util')
+util_dir = os.path.join(os.getenv('CHPL_HOME'), 'util', 'chplenv')
 sys.path.insert(0, os.path.abspath(util_dir))
 
 from chplenv import chpl_home_utils
-from chplenv import chpl_3p_re2_configs
+from chplenv import chpl_regexp
 
 def get():
     third_party = chpl_home_utils.get_chpl_third_party()
-    uniq_cfg_path = chpl_3p_re2_configs.get_uniq_cfg_path()
+    uniq_cfg_path = chpl_regexp.get_uniq_cfg_path()
     re2_install_dir = os.path.join(third_party, 're2', 'install',
                                    uniq_cfg_path)
     return re2_install_dir
