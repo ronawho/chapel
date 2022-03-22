02f694b35797386cbd63bd15cafaa275397511b4
Elliot Ronaghan <ronawho@gmail.com>
2022-03-22 08:47:11 -0500

    Submit multiple jobs to avoid hitting slurm array limit
    Overall Questions/TODOs:
     - How do we track progress? `squeue` looking at ids pending? `scontrol show`?
       Have each job write to a different file to track successful completion?
     - Better to use job array or individual? sruns (job array is better for
       scheduler, but srun would make it easier to track completion
     - Right way to cancel a job (need to propagate scancel)
     - What to do if lacking log information to estimate runtime(query number and
       size of .chpl files?)
     - Do we need larger submissions instead of just directories? Startup overhead
       doesn't seem bad now, but may be higher with container?
    
    Signed-off-by: Elliot Ronaghan <ronawho@gmail.com>

--- test/paratest.sbatch.py
+++ test/paratest.sbatch.py
@@ -14,27 +14,48 @@ import fileutils
 
 chpl_home = os.getenv('CHPL_HOME')
 
-def create_batch_file(test_dirs):
+def create_batch_file(test_dirs, start):
     process_template = os.path.join(chpl_home, 'util', 'build_configs', 'cray-internal', 'process-template.py')
+    keys_start = "START={}".format(start)
     keys_len = "TEST_ARR_LEN={}".format(len(test_dirs)-1)
     keys_arr = "TEST_ARR={}".format(' '.join('"{}"'.format(test_dir) for test_dir in test_dirs))
-    subprocess.check_output([process_template, '--template', 'paratest.batch.template', '--output', 'paratest.batch', keys_len, keys_arr])
+    subprocess.check_output([process_template, '--template', 'paratest.batch.template', '--output', 'paratest.batch', keys_start, keys_len, keys_arr])
 
-def run_batch_file():
-    print(subprocess.check_output(['sbatch', '--wait', 'paratest.batch']))
+def chunks(l, n):
+    n = max(1, n)
+    return (l[i:i+n] for i in range(0, len(l), n))
 
-test_dirs = fileutils.find_chpl_test_dirs(sys.argv[1])
-print(len(test_dirs))
-exit()
-test_dirs = test_dirs[0:1000]
-create_batch_file(test_dirs)
+def run_batch_file(test_dirs, deps=False):
+    args = ['sbatch', '--parsable']
+    if deps:
+        args.append('--wait')
+        args.append('--dependency')
+        args.append(','.join(deps))
+    args.append('paratest.batch')
+    print(args)
+    out = subprocess.check_output(args).decode().strip()
+    return out
+
+
+output_file = 'paratest.log'
+test_dirs = fileutils.find_chpl_test_dirs(sys.argv[1], output_file)
+
+print(test_dirs[0:100])
 
 os.environ['CHPL_TEST_LIMIT_RUNNING_EXECUTABLES'] = 'yes'
 os.environ['CHPL_RT_OVERSUBSCRIBED'] = 'yes'
-run_batch_file()
+
+deps = []
+start = 0
+for test_dirs2 in chunks(test_dirs[:-1], 1000):
+    create_batch_file(test_dirs2, start)
+    deps.append(run_batch_file(test_dirs2))
+    start += len(test_dirs2)
+create_batch_file([test_dirs[-1]], start)
+run_batch_file([test_dirs[-1]], deps)
+
 
 input_files = ['start_test_log_{}'.format(i) for i in range(len(test_dirs))]
-output_file = 'paratest.log'
 fileutils.merge_files(input_files, output_file)
 
 print(summarize.create_summary(output_file))
