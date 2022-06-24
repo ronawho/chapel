43c24533cdb161823099a7cde9ce22c984ac83b7
Elliot Ronaghan <ronawho@gmail.com>
2022-06-24 10:38:52 -0500

    For small AMs without a remote buffer use AM payload as buffer
    Signed-off-by: Elliot Ronaghan <ronawho@gmail.com>

--- runtime/include/chpl-tasks.h
+++ runtime/include/chpl-tasks.h
@@ -65,6 +65,7 @@ typedef struct chpl_task_bundle {
   int filename;
   c_sublocid_t requestedSubloc;
   size_t nbytes;
+  size_t aggOffset;
   chpl_fn_int_t requested_fid;
   chpl_fn_p requested_fn;
   chpl_taskID_t id;
