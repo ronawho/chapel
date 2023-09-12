a5b425ea049e44ca3b00a2448486d0eda98e9de2
Elliot Ronaghan <ronawho@gmail.com>
2023-09-12 12:08:36 -0400

    See if we can get pbs-gasnetrun_ibv working under jenkins
    Signed-off-by: Elliot Ronaghan <ronawho@gmail.com>

--- util/cron/test-perf.cray-cs-hdr.arkouda.bash
+++ util/cron/test-perf.cray-cs-hdr.arkouda.bash
@@ -8,14 +8,17 @@ export CHPL_TEST_PERF_CONFIG_NAME='16-node-cs-hdr'
 export CHPL_NIGHTLY_TEST_CONFIG_NAME="perf.cray-cs-hdr.arkouda"
 
 # setup arkouda
-source $CWD/common-arkouda.bash
-export ARKOUDA_NUMLOCALES=16
+#source $CWD/common-arkouda.bash
+#export ARKOUDA_NUMLOCALES=16
 
 module list
 
-# setup for CS perf (gasnet-large, gnu, 128-core Rome)
-source $CWD/common-cray-cs.bash
-export CHPL_LAUNCHER_PARTITION=rome64Share
+# setup for apollo
+source $CWD/common-hpe-apollo.bash
+unset CHPL_TEST_LAUNCHCMD
+export CHPL_LAUNCHER=pbs-gasnetrun_ibv
+export CHPL_LAUNCHER_CORES_PER_LOCALE=144
+export CHPL_LAUNCHER_QUEUE=f2401THP
 export CHPL_TARGET_CPU=none
 
 module list
@@ -23,5 +26,4 @@ module list
 export GASNET_PHYSMEM_MAX="9/10"
 nightly_args="${nightly_args} -no-buildcheck"
 
-test_nightly
-sync_graphs
+$CWD/nightly -cron -examples
